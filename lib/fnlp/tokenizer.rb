cfa89f2a890407c9f24f4ce357f6b4ff935b5460
AKIYOSHI, Masamichi <akiyoshi.masamichi@bsearchtech.com>
2015-01-27 12:58:47 +0900

    return empty array for blank string.

--- lib/fnlp/tokenizer.rb
+++ lib/fnlp/tokenizer.rb
@@ -2,15 +2,24 @@ module FNLP
   import "org.fnlp.nlp.cn.CNFactory"
 
   class Tokenizer
-    def initialize
-      @factory = CNFactory.getInstance("#{HOME}/models")
+    def initialize(models=nil)
+      @tokenizer = CNFactory.getInstance(models || "#{HOME}/models")
     end
 
     def tokenize(string)
-      words, tags = @factory.tag(string)
+      return [] if empty?(string)
+
+      words, tags = @tokenizer.tag(string)
+      return [] if empty?(words)
+
       words.zip(tags).map do |word_tag|
         Token.new(*word_tag)
       end
+    end
+
+    private 
+    def empty?(s)
+      !s || (s.is_a?(String) && s.strip == "") || (s.is_a?(Array) && s.empty?)
     end
   end
 end
